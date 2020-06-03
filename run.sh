352ab7ee285717a9ef1da3527688d51799d4cafa
Aleksei Zakharov <zaharov@selectel.ru>
2020-06-03 16:36:04 +0300

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-HOST=$1
-DIR=$2
+case "$#" in
+  0 )
+    HOST='localhost'
+    DIR="/home/$(logname)"
+    ;;
+  1 )
+    if [[ "help" == "${1}" ]]; then
+      echo "Usage: ${0} HOST PATH"
+      echo "will do scp HOST:PATH"
+      exit 0
+    fi
+    HOST="${1}"
+    DIR="/home/$(logname)"
+    ;;
+  2 )
+    HOST="${1}"
+    DIR="${2}"
+    ;;
+  * )
+    echo "Accept only two arguments: ${0} host path"
+    exit 1
+    ;;
+esac
 
 set +x
 ssh "${HOST}" 'rm ./tests'
