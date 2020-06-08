c6485f95f9f669790153933adbbd23ef3d167679
Aleksei Zakharov <zaharov@selectel.ru>
2020-06-08 14:15:32 +0300

    Add plot-all.sh

---
+++ plot-all.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+case "$#" in
+  0 )
+    PLOT_DIRS="$(ls -d ./results/*)"
+    ;;
+  * )
+    PLOT_DIRS="${*}"
+    ;;
+esac
+
+OUTPUT_DIR='./plots'
+
+if [[ -d ${OTPUT_DIR} ]]; then
+  echo "${OUTPUT_DIR} is already exists"
+  echo "Please, delete and re-run ${0}"
+  exit 1
+fi
+mkdir -p ${OUTPUT_DIR}
+
+PLOT_FILES=''
+
+for d in ${PLOT_DIRS}; do
+  for f in $(ls -d ${d}/*); do
+    file=$(basename ${f})
+    if [[ "${PLOT_FILES}" != *"${file}"* ]]; then
+      PLOT_FILES=${PLOT_FILES}" "${file}
+    fi
+  done
+done
+
+for f in ${PLOT_FILES}; do
+  PLOT_WITH_PATHS=''
+  for d in ${PLOT_DIRS}; do
+    PLOT_WITH_PATHS=${PLOT_WITH_PATHS}" "${d}/${f}
+  done
+  if [[ ${f} == *iops.[0-9]*.log* ]]; then
+    ./plot.py -i ${PLOT_WITH_PATHS} --interval 1000 --ylabel IO/s --value-divider 1 --median --sum-bucket 1000 -o ${OUTPUT_DIR}/${f}_median.svg
+  elif [[ ${f} == *bw.[0-9]*.log* ]]; then
+    ./plot.py -i ${PLOT_WITH_PATHS} --interval 1000 --ylabel MiB/s --value-divider 1024 --median -o ${OUTPUT_DIR}/${f}_median.svg
+  elif [[ ${f} == *clat.[0-9]*.log* ]]; then
+    ./plot.py -i ${PLOT_WITH_PATHS} --interval 10000 -o ${OUTPUT_DIR}/${f}.svg
+    ./plot.py -i ${PLOT_WITH_PATHS} --median --interval 10000 -o ${OUTPUT_DIR}/${f}_median.svg
+  fi
+done
