f909447af96101574afab9243bf3e501b7e7c859
Aleksei Zakharov <zakharov.a.g@yandex.ru>
2020-07-25 22:34:53 +0300

    Use plotly (#1)
    * Move to plotly + plotly-orca

--- plot.py
+++ plot.py
@@ -3,13 +3,9 @@ import argparse
 import sys
 import os
 import csv
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 import numpy as np
-
-# This is just a list for boxplot colors. Why not?
-bplot_colors = ['red', 'blue', 'green', 'lightblue', 'lightgreen', 'pink', 'burlywood', 'chartreuse']
-flier_colors = ['pink', 'lightblue', 'lightgreen', 'blue', 'green', 'red', 'burlywood', 'chartreuse']
-median_colors = ['blue', 'red', 'lightgreen', 'pink', 'burlywood', 'chartreuse', 'green', 'lightblue']
+import pandas as pd
 
 def get_time_value(filename):
     f = open(filename,"r+")
@@ -28,105 +24,44 @@ def count_lines(filename):
     return count
 
 def work(args):
-    figure, axes = plt.subplots(nrows=1,ncols=1,figsize=(9,4))
-    if not args.median: # Do we need better solution?
-        bplot_color_choice = 0
-
-    bplot_boxes = list()
-    bplot_legends = list()
+    fig = go.Figure()
+    header = ["time","value","op","bs"]
 
     for input_file in args.input:
-        bound = args.interval
-        sum_bound = args.sum_bucket
-        values_plot = list() # Values of Y to plot
-        labels_plot = list() # Labels for X to plot on
-        values = list() # Raw values from file
-        lines_count = count_lines(input_file)
-        for timestamp, v, line_num in get_time_value(input_file):
-            v = v / args.value_divider
-            if (timestamp > bound): # |---s-----bt <- t is after the bound. Cleanup and prepare for plot
-                if not args.msec:
-                    labels_plot.append(bound/1e3)
-                else:
-                    labels_plot.append(bound)
-                if args.median: # Count median here if needed
-                    values_plot.append(np.percentile(values,50))
-                else:
-                    values_plot.append(np.array(values).astype(np.float))
-                values.clear()
-                bound = bound + args.interval
-                sum_bound = args.sum_bucket
-                values.append(v)
-            elif (sum_bound):
-                if (timestamp <= bound - args.interval + sum_bound): # |-t-s-----b <- we are between '|' and 's'
-                    try:
-                        values[-1] += v
-                    except IndexError: # The only reason is empty list
-                        values.append(v)
-                else: # If we're out of bucket, then create set new sum_bound
-                    sum_bound += args.sum_bucket
-                    if (sum_bound > bound):
-                        sum_bound = bound
-                    values.append(v) # And append value to new bucket
-            else: # We are not summing and not over the bound. Just add new value
-                values.append(v)
+        df = pd.read_csv(input_file,names=header)
+        df['time_round'] = round(df['time']/args.interval)*args.interval+args.interval
+        df['value'] /= args.value_divider
 
-            if (line_num == lines_count): # We done. This was the last iteration
-                if not args.msec:
-                    labels_plot.append(timestamp/1e3)
-                else:
-                    labels_plot.append(timestamp)
-                if args.median:
-                    values_plot.append(np.percentile(values, 50))
-                else:
-                    values_plot.append(np.array(values).astype(np.float))
+        if not args.msec:
+            df['time_round'] /= 1e3
 
         if args.median:
-            axes.plot(labels_plot,np.array(values_plot),'o-',label=input_file)
+            if args.per_second:
+                df = df.groupby('time_round').sum().reset_index()
+                df['value'] /= args.interval/1e3
+            else:
+                df = df.groupby('time_round').median().reset_index()
+            fig.add_trace(go.Scatter(x=df['time_round'], y=df['value'],
+                                mode="lines",name=input_file))
         else:
-            locs, ticks = plt.xticks()
-            manage_ticks = False
-            if len(locs) < len(labels_plot):
-                manage_ticks = True
-            bp = axes.boxplot(
-                values_plot,
-                vert=True,
-                manage_ticks=manage_ticks,
-                patch_artist=True,
-                labels=labels_plot, # Plot name as filename
-                whis=[1,99],
-                boxprops=dict(
-                    facecolor=bplot_colors[bplot_color_choice]),
-                whiskerprops=dict(
-                    color=bplot_colors[bplot_color_choice]),
-                flierprops=dict(
-                    color=bplot_colors[bplot_color_choice],
-                    markeredgecolor=flier_colors[bplot_color_choice],
-                    marker='+'),
-                medianprops=dict(
-                    color=median_colors[bplot_color_choice+1])
-                )
-            bplot_boxes.append(bp["boxes"][0])
-            bplot_legends.append(input_file)
-            bplot_color_choice += 1
+            fig.add_trace(go.Box(x=df['time_round'], y=df['value'],
+                                name=input_file))
 
-    axes.set_title(args.title)
-    if not args.median:
-        axes.legend(bplot_boxes,bplot_legends)
-    else:
-        plt.legend()
-    plt.ylabel(args.ylabel)
-    plt.xlabel(args.xlabel)
+    fig.update_layout(title=args.title,
+                    xaxis_title=args.xlabel,
+                    yaxis_title=args.ylabel,
+                    legend=dict(
+                        font=dict(
+                            size=24
+                        )))
     if (args.top_limit):
-        plt.ylim(bottom=0,top=args.top_limit)
-    else:
-        plt.ylim(bottom=0)
-    plt.grid(True)
+        fig.update_yaxes(range=[0,args.top_limit])
+    fig.update_yaxes(rangemode="tozero")
 
     if args.output:
-        plt.savefig(args.output)
+        fig.write_image(args.output,width="1920",height="1080")
     else:
-        plt.show()
+        fig.show()
 
 def main():
     parser = argparse.ArgumentParser(description="Plot stuff")
@@ -134,7 +69,7 @@ def main():
     parser.add_argument('--output','-o',type=str,help="Write char to this output file")
     parser.add_argument('--interval',type=int,help="Plot boxplot on interval (msec)",default=30000)
     parser.add_argument('--title','-t',type=str,help="Plot title",default="FIO results")
-    parser.add_argument('--sum-bucket',type=int,help="Summarize values on this interval and treat it as a value to plot",default=0)
+    parser.add_argument('--per-second',action='store_true',help="Count per-second median value for the given interval (eg IO/s graph)")
     parser.add_argument('--median',action='store_true',help="Plot medians without boxplot")
     parser.add_argument('--msec',action='store_true',help="Show time in milliseconds instead of seconds")
     parser.add_argument('--ylabel',type=str,help="Set this Y-label",default="latency (msec)")
